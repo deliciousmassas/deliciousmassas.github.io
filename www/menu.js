4a0431394c618f9690a209faf90b368c463b5337
Adalberto Junior <adalrsjr1@gmail.com>
2018-02-15 02:55:30 -0200

    fix navbar

--- www/menu.js
+++ www/menu.js
@@ -1,6 +1,24 @@
-$(function () {
+// $(function () {
+    
+
+    
+// });
 $(document).scroll(function () {
     var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
+    $nav.toggleClass('scrolled', $(this).scrollTop() > 0 /*$nav.height()*/);
+    $nav.toggleClass('toggle-flag', $(this).scrollTop() > 0);
 });
+
+$('#collapsibleNavId').on('show.bs.collapse', function (e) {
+    // Action to execute once the collapsible area is expanded
+    var $nav = $(".navbar-fixed-top");
+    $nav.addClass("scrolled")
+});
+
+$('#collapsibleNavId').on('hide.bs.collapse', function (e) {
+    // Action to execute once the collapsible area is expanded
+    var $nav = $(".navbar-fixed-top");
+    if(!$nav.hasClass("toggle-flag")) {
+        $nav.removeClass("scrolled")
+    }
+}); 
