c78ae7b48d516308f93a23201a46bfa0d016f924
Adalberto Junior <adalrsjr1@gmail.com>
2018-02-16 01:22:24 -0200

    building contact section

--- www/menu.js
+++ www/menu.js
@@ -22,3 +22,12 @@ $('#collapsibleNavId').on('hide.bs.collapse', function (e) {
         $nav.removeClass("scrolled")
     }
 }); 
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.2),
+        zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
