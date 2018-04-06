f5703d706dadf4e56e4dad6f5a585c5aa1896e3f
Adalberto Junior <adalrsjr1@gmail.com>
2018-04-06 12:04:41 -0300

    new mobile project

---
+++ app/mobile/app/src/main/java/deliciousmassas/com/br/delicious/OrderFormActivity.kt
@@ -0,0 +1,12 @@
+package deliciousmassas.com.br.delicious
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_form)
+    }
+}
